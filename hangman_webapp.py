71ad3721ef6d30922c11443e7ca67f5f3611e449
ruoxin <clarkyangruoxin@outlook.com>
2020-04-13 23:04:38 -0400

    webapp

--- hangman_webapp.py
+++ hangman_webapp.py
@@ -7,47 +7,48 @@ from flask import Flask, render_template, request
 import hangman_app
 app = Flask(__name__)
 
-def word_so_far(answer, guesses):
-    return str.join('', [x if x in guesses else '_' for x in answer])
-
+word = hangman_app.generate_random_word()
 global state
 state = {'guesses':[],
-         'word':"interesting",
-		 'word_so_far':"",
-         'guesses_left': 6,
-		 'done': False}
+         'word':'N/A',
+         'word_so_far':'',
+         'chances_left': 6,
+         'done':False}
+
+def word_so_far(ans, guesses):
+    return str.join('', [i if i in guesses else '-' for i in ans])
 
 @app.route('/')
 @app.route('/main')
 def main():
-	return render_template('hangman.html')
+    return render_template('hangman.html')
 
 @app.route('/start')
 def play():
     global state
     word = hangman_app.generate_random_word()
     state['word']= word
-    state['word_so_far'] = '_' * len(word)
+    state['word_so_far'] = '-' * len(word)
     state['chances_left'] = 6
     state['guesses'] = []
     return render_template("start.html",state=state)
 
-
-
 @app.route('/play',methods=['GET','POST'])
-
-
 def hangman():
+    """ plays hangman game """
     global state
     if request.method == 'GET':
         return play()
 
     elif request.method == 'POST':
+        letter = request.form['guess']
         state['guesses'] += [letter]
         if letter not in state['word']:
-            state['guesses_left'] -= 1
-            state['word_so_far'] = word_so_far(state['word'], state['guesses'])
+            state['chances_left'] -= 1
+
+        state['word_so_far'] = word_so_far(state['word'], state['guesses'])
         return render_template('play.html',state=state)
+
 
 @app.route('/about')
 def about():
